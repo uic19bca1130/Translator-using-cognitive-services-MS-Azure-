e77c3919d85c1ae6ca1f60c69ac678df9eaf0210
Ankush <128672958+uic19bca1130@users.noreply.github.com>
2023-07-25 17:15:26 +0530

    new ad languages

--- Translator/Program.cs
+++ Translator/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
@@ -8,111 +10,112 @@ using Newtonsoft.Json;
 
 class Program
 {
-    private static readonly string computerVisionKey = "ee7e74ec080041d79ea9a2e15482e61b";
-    private static readonly string computerVisionEndpoint = "https://msvisionui.cognitiveservices.azure.com/";
-    private static readonly string translationKey = "e1000ad9e30a471ea46305cf5d712117";
-    private static readonly string translationEndpoint = "https://api.cognitive.microsofttranslator.com/";
-    private static readonly string location = "centralindia";
-
-    static async Task Main(string[] args)
-    {
-        // Path to the local image file
-        string imagePath = @"D:\Ankush projects\Translator console\arabic (1)(2).png";
+	private static readonly string computerVisionKey = "0e76869a9548428cba23bb73738c8c52";
+	private static readonly string computerVisionEndpoint = "https://msvisionui.cognitiveservices.azure.com/";
+	private static readonly string translationKey = "781b0ec20356402594ba03f44df18167";
+	private static readonly string translationEndpoint = "https://api.cognitive.microsofttranslator.com";
+	private static readonly string location = "centralindia";
 
 
-        // Authenticate Computer Vision client
-        var computerVisionClient = AuthenticateComputerVision(computerVisionEndpoint, computerVisionKey);
+	static async Task Main(string[] args)
+	{
+		// Path to the local image file
+		string imagePath = @"C:\Users\Ankush\Downloads\Amazing-Facts-About-the-Sanskrit-Language-that-You-Didnt-Know.jpg";
 
-        // Analyze the image and extract text
-        var extractedText = await ExtractTextFromImage(computerVisionClient, imagePath);
+		// Authenticate Computer Vision client
+		var computerVisionClient = AuthenticateComputerVision(computerVisionEndpoint, computerVisionKey);
 
-        // Translate the extracted text (if needed)
-        var translatedText = await TranslateText(extractedText, "ar", "en");
+		// Analyze the image and extract text
+		var extractedText = await ExtractTextFromImage(computerVisionClient, imagePath);
 
-        Console.WriteLine($"Extracted Text: {extractedText}");
-        Console.WriteLine($"Translated Text: {translatedText}");
-    }
+		// Translate the extracted text (if needed)
+		var translatedText = await TranslateText(extractedText, "hi", "en");
 
-    public static ComputerVisionClient AuthenticateComputerVision(string endpoint, string key)
-    {
-        ComputerVisionClient client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(key)) { Endpoint = endpoint };
-        return client;
-    }
+		Console.WriteLine($"Extracted Text: {extractedText}");
+		Console.WriteLine($"Translated Text: {translatedText}");
+	}
 
-    public static async Task<string> ExtractTextFromImage(ComputerVisionClient client, string imagePath)
-    {
-        using (Stream imageStream = File.OpenRead(imagePath))
-        {
-            var textHeaders = await client.ReadInStreamAsync(imageStream, "ar");
+	public static ComputerVisionClient AuthenticateComputerVision(string endpoint, string key)
+	{
+		ComputerVisionClient client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(key)) { Endpoint = endpoint };
+		return client;
+	}
 
-            // Get the operation ID
-            string operationId = textHeaders.OperationLocation.Split('/').Last();
+	public static async Task<string> ExtractTextFromImage(ComputerVisionClient client, string imagePath)
+	{
+		using (Stream imageStream = File.OpenRead(imagePath))
+		{
+			var textHeaders = await client.ReadInStreamAsync(imageStream, language: "hi");
 
-            // Wait for the operation to complete
-            while (true)
-            {
-                var result = await client.GetReadResultAsync(Guid.Parse(operationId));
+			// Get the operation ID
+			string operationId = textHeaders.OperationLocation.Split('/').Last();
 
-                if (result.Status == OperationStatusCodes.Succeeded || result.Status == OperationStatusCodes.Failed)// reprsent  the possible values for the status of an operation.
-                {
-                    var extractedText = new StringBuilder();// ||or logical operator condition will be true
-                    foreach (var page in result.AnalyzeResult.ReadResults)
-                    {
-                        foreach (var line in page.Lines)
-                        {
-                            foreach (var word in line.Words)
-                            {
-                                extractedText.Append(word.Text);
-                                extractedText.Append(" ");
-                            }
-                        }
-                    }
+			// Wait for the operation to complete
+			while (true)
+			{
+				var result = await client.GetReadResultAsync(Guid.Parse(operationId));
 
-                    return extractedText.ToString().Trim();
-                }
+				if (result.Status == OperationStatusCodes.Succeeded || result.Status == OperationStatusCodes.Failed)
+				{
+					var extractedText = new StringBuilder();
+					foreach (var page in result.AnalyzeResult.ReadResults)
+					{
+						foreach (var line in page.Lines)
+						{
+							foreach (var word in line.Words)
+							{
+								extractedText.Append(word.Text);
+								extractedText.Append(" ");
+							}
+						}
+					}
 
-                await Task.Delay(1000);//1000miliseconds in the program's execution
-            }
-        }
-    }
-    public static async Task<string> TranslateText(string text, string sourceLanguage, string targetLanguage)
-    {
-        using (var client = new HttpClient())
-        {
-            string route = $"/translate?api-version=3.0&from={sourceLanguage}&to={targetLanguage}";
-            string uri = translationEndpoint + route;
+					return extractedText.ToString().Trim();
+				}
 
-            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", translationKey);
-            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Region", location);
+				await Task.Delay(1000);
+			}
+		}
+	}
 
-            var requestBody = new List<object>() { new { Text = text } };
-            var content = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json");
-            var response = await client.PostAsync(uri, content);
+	public static async Task<string> TranslateText(string text, string sourceLanguage, string targetLanguage)
+	{
+		using (var client = new HttpClient())
+		{
+			string route = $"/translate?api-version=3.0&from={sourceLanguage}&to={targetLanguage}";
+			string uri = translationEndpoint + route;
 
-            response.EnsureSuccessStatusCode();
+			client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", translationKey);
+			client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Region", location);
 
-            string result = await response.Content.ReadAsStringAsync();
+			var requestBody = new List<object>() { new { Text = text } };
+			var content = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json");
+			var response = await client.PostAsync(uri, content);
 
-            var translationResponse = JsonConvert.DeserializeObject<List<TranslationResponse>>(result);
-            var translations = translationResponse?[0]?.Translations;
-            if (translations != null && translations.Count > 0)
-            {
-                var translatedText = translations[0]?.Text;
-                return translatedText ?? string.Empty;
-            }
+			response.EnsureSuccessStatusCode();
 
-            return string.Empty; // Add this return statement
-        }
-    }
+			string result = await response.Content.ReadAsStringAsync();
+
+			var translationResponse = JsonConvert.DeserializeObject<List<TranslationResponse>>(result);
+			var translations = translationResponse?[0]?.Translations;
+			if (translations != null && translations.Count > 0)
+			{
+				var translatedText = translations[0]?.Text;
+				return translatedText ?? string.Empty;
+			}
+
+			return string.Empty;
+		}
+	}
 }
 
 public class TranslationResponse
 {
-    public List<Translation>? Translations { get; set; }
+	public List<Translation> Translations { get; set; }
 }
 
 public class Translation
 {
-    public string? Text { get; set; }
-    public string? To { get; set; }
+	public string Text { get; set; }
+	public string To { get; set; }
 }
